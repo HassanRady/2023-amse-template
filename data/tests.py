d5dccfe745919441479864dab65a7ae18f95b47a
HassanRady <hassan.khaled.rady@gmail.com>
2023-06-14 12:31:11 +0200

    refactoring

--- data/tests.py
+++ data/tests.py
@@ -15,58 +15,58 @@ station_loader = StationLoader()
 station_loader.load_stations()
 sample_station = station_loader.stations_list[10].EVA_NR
 
+SqliteClient.db_engine.execute("""
+    CREATE TABLE IF NOT EXISTS stations (
+EVA_NR int,
+DS100 text,
+IFOPT text,
+NAME text,
+Verkehr text,
+Laenge text,
+Breite text,
+Betreiber_Name text,
+Betreiber_Nr int,
+Status text,
+PRIMARY KEY (EVA_NR)
+);
+""")
+SqliteClient.db_engine.execute("""
+    
+CREATE TABLE IF NOT EXISTS train_plan (
+EVA_NR int,
+stop_id text,
+trip_type text,
+train_type text,
+train_number text,
+train_line text,
+platform text,
+next_stations text,
+passed_stations text,
+arrival text,
+departure text,
+FOREIGN KEY (EVA_NR) REFERENCES stations(EVA_NR)
+);
+""")
+SqliteClient.db_engine.execute("""
+
+CREATE TABLE IF NOT EXISTS plan_change(
+EVA_NR int,
+stop_id text,
+next_stations text,
+passed_stations text,
+arrival text,
+departure text,
+platform text,
+FOREIGN KEY (EVA_NR) REFERENCES stations(EVA_NR),
+FOREIGN KEY (stop_id) REFERENCES train_plan(stop_id),
+PRIMARY KEY (EVA_NR, stop_id)
+);
+""")
 
 class TestDataPipeline(TestCase):
 
     def test_pipeline(self):
 
-        SqliteClient.db_engine.execute("""
-            CREATE TABLE IF NOT EXISTS stations (
-    EVA_NR int,
-    DS100 text,
-    IFOPT text,
-    NAME text,
-    Verkehr text,
-    Laenge text,
-    Breite text,
-    Betreiber_Name text,
-    Betreiber_Nr int,
-    Status text,
-    PRIMARY KEY (EVA_NR)
-);
-  """)
-        SqliteClient.db_engine.execute("""
-          
-CREATE TABLE IF NOT EXISTS train_plan (
-    EVA_NR int,
-    stop_id text,
-    trip_type text,
-    train_type text,
-    train_number text,
-    train_line text,
-    platform text,
-    next_stations text,
-    passed_stations text,
-    arrival text,
-    departure text,
-    FOREIGN KEY (EVA_NR) REFERENCES stations(EVA_NR)
-);
-""")
-        SqliteClient.db_engine.execute("""
-   
-CREATE TABLE IF NOT EXISTS plan_change(
-        EVA_NR int,
-        stop_id text,
-        next_stations text,
-        passed_stations text,
-        arrival text,
-        departure text,
-        platform text,
-        FOREIGN KEY (EVA_NR) REFERENCES stations(EVA_NR),
-        FOREIGN KEY (stop_id) REFERENCES train_plan(stop_id),
-        PRIMARY KEY (EVA_NR, stop_id)
-);
-        """)
 
         self.station_loader = StationLoader()
         self.api_client = ApiClient()
