8f5ee9fbfa63c76517957533ab9f589f6c715fa7
HassanRady <hassan.khaled.rady@gmail.com>
2023-06-24 18:16:40 +0200

    refactoring

--- data/weather_data_pipeline.py
+++ data/weather_data_pipeline.py
@@ -1,3 +1,10 @@
-from weather_data.data_getter import get_raw_data_to_sqlite
+from weather_data.data_getter import get_raw_data
+from weather_data.data_processor import make_unique
+from config import config
+from database_client import SqliteClient
 
-get_raw_data_to_sqlite()
+df = get_raw_data()
+
+df = make_unique(df)
+
+df.to_sql(config.weather_data.RAW_WEATHER_DATA_TABLE, SqliteClient.db_engine, if_exists='append', index=False)
