fe1e7f14d5621be3a25126f6a73178bff8d6e437
HassanRady <hassan.khaled.rady@gmail.com>
2023-06-28 09:44:17 +0200

    handling api error responds

--- data/db_api_data_pipeline.py
+++ data/db_api_data_pipeline.py
@@ -1,3 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(''))
+
+from logger import get_file_logger
+_logger = get_file_logger(__name__, 'debug')
+
 from database_client import SqliteClient
 from deutsche_bahn_api.api_caller import ApiClient
 from deutsche_bahn_api.timetable_retrieval import TimeTableHandler
@@ -10,8 +17,6 @@ station_helper.load_stations()
 timetable_handler = TimeTableHandler()
 
 
-
-
 def start_full_pipeline(sample):
     for station in station_helper.stations_list:
         station.insert_to_db(SqliteClient.db_engine)
@@ -19,13 +24,21 @@ def start_full_pipeline(sample):
 
 def start_api_pipeline(sample):
     for station in station_helper.stations_list[:sample]:
+        print(station.EVA_NR)
         response = api_client.get_current_hour_station_timetable(
             station.EVA_NR)
+
+        # _logger.debug(response.text)
+        if "Request Rejected" in response.text:
+            print(f"Bad station data {station.EVA_NR}")
+            continue
+            
         if response.status_code != 200:
             continue
         else:
             trains_in_this_hour = timetable_handler.get_timetable_data(
                 response)
+            
             for train_plan in trains_in_this_hour:
                 if train_plan.arrival == "N/A" or train_plan.departure == "N/A":
                     continue
@@ -34,9 +47,16 @@ def start_api_pipeline(sample):
     for station in station_helper.stations_list[:sample]:
         response = api_client.get_all_timetable_changes_from_station(
             station.EVA_NR)
+        
+        if "Request Rejected" in response.text:
+            print(f"Bad station data {station.EVA_NR}")
+            continue
+
+
         if response.status_code != 200:
             continue
         plans_change = timetable_handler.get_timetable_changes_data(response)
+
         for plan_change in plans_change:
             if plan_change.arrival == "N/A" or plan_change.departure == "N/A":
                 continue
@@ -45,4 +65,4 @@ def start_api_pipeline(sample):
     SqliteClient.db_engine.close()
 
 if __name__ == "__main__":
-    start_full_pipeline(sample=2)
+    start_full_pipeline(sample=30)
