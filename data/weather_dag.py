4527c762106abd889b2bf7f039e324b0321fdcb1
HassanRady <hassan.khaled.rady@gmail.com>
2023-06-28 15:01:46 +0200

    dag for weather data

---
+++ data/weather_dag.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from pathlib import Path
+
+from weather_data_pipeline import start_pipeline
+
+
+with DAG(dag_id="SAKI-WEATHER-DATA",
+         start_date=datetime(2023, 5, 25),
+         schedule="@daily",
+         catchup=False) as dag:
+    task1 = PythonOperator(
+        task_id="weather_data_getter",
+        python_callable=start_pipeline)
+
+task1
